06227f636a4f2d8918d6176452b58744377b92dd
Steve Brunton <steve.brunton@oracle.com>
2018-05-07 15:55:40 -0700

    figuring out how the SDK works a little more

--- cmd/root.go
+++ cmd/root.go
@@ -35,17 +35,21 @@ import (
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	"github.com/nshttpd/oci-tool/oci"
+	"github.com/oracle/oci-go-sdk/common"
 )
 
-const defaultProfile = "DEFAULT"
+const (
+	defaultProfile = "DEFAULT"
+	defaultConfig = "/.oci/config"
+)
 
 var (
 	cfgFile string
 	region string
 	profile string
 	compartment string
+	config common.ConfigurationProvider
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -64,8 +68,19 @@ filter to only a single compartment if needed. It will also get the vnics and
 addresses for the hosts.
 `,
 	PersistentPreRun: func(cobra *cobra.Command, args []string) {
+		if cobra.Flag("region").Value.String() == "" {
+			fmt.Printf("region must be supplied as a parameter\n")
+			os.Exit(1)
+		}
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 		v := cobra.Flag("profile").Value
-		oci.CreateClient(v.String())
+		f := cobra.Flag("config").Value
+
+		config = oci.CreateConfig(home + f.String(), v.String())
 	},
 }
 
@@ -79,12 +94,10 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.oci/config)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", defaultConfig, "config file (default is $HOME/.oci/config)")
 	rootCmd.PersistentFlags().StringVar(&region, "region", "", "OCI region")
 	rootCmd.PersistentFlags().StringVar(&profile, "profile", defaultProfile, "Config File Profile")
 	rootCmd.PersistentFlags().StringVar(&compartment, "compartment", "", "Compartment Name")
@@ -94,28 +107,3 @@ func init() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".foo" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".oci/config")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
-}
--- oci/client.go
+++ oci/client.go
@@ -1,7 +1,16 @@
 package oci
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/oracle/oci-go-sdk/common"
+)
 
-func CreateClient(profile string) {
-	fmt.Printf("profile is : %s\n", profile)
+func CreateConfig(file string, profile string) common.ConfigurationProvider {
+	config, err := common.ConfigurationProviderFromFileWithProfile(file, profile, "")
+	if err != nil {
+		fmt.Printf("error creating config for profile %s from file %s\n", profile, file)
+		fmt.Printf("error : %v\n", err)
+		config = nil
+	}
+	return config
 }
